b340de05c6fec76140553da758c001b91796ab47
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-02-12 18:13:24 +0100

    hero Title responsiveness fixed

--- src/app/components/hero/index.jsx
+++ src/app/components/hero/index.jsx
@@ -9,35 +9,40 @@ const Hero = () => {
         <Navbar />
         <div className="lowerSubHeroWrapper">
           <div className="leftContainer">
-            <div className="heroTitleWrapper">
-              <h2 className="heroTxt">
-                <span className="hey">Hey,</span> <span className="breakTxt"><br /></span>
-                <span className="name">I’m Sangita</span>
-              </h2>
-              <Image
-                src={`/images/check.svg`}
-                width={43}
-                height={43}
-                alt="verified icon"
-                className="checkMark hero"
-              />
-            </div>
-            <p className="heroDesc">
-              I’m a Corporate Trainer, Academician, and{" "}
-              <span className="bolder">Founder of ProStrategic-H</span>, with 18+ years of experience in
-              leadership development, communication mastery, and
-              transformational learning. I help professionals and organizations
-              unlock their true potential.
-            </p>
-            <div className="heroCta">
-              <p>Connect Now</p>
-              <Image
-                src={`/images/cta-arrow.svg`}
-                width={32}
-                height={32}
-                alt="arrow icon"
-                className="ctaIcon"
-              />
+            <div className="innerLeftContainer">
+              <div className="heroTitleWrapper">
+                <h2 className="heroTxt">
+                  <span className="hey">Hey,</span>{" "}
+                  <span className="breakTxt">
+                    <br />
+                  </span>
+                  <span className="name">I’m Sangita</span>
+                </h2>
+                <Image
+                  src={`/images/check.svg`}
+                  width={43}
+                  height={43}
+                  alt="verified icon"
+                  className="checkMark hero"
+                />
+              </div>
+              <p className="heroDesc">
+                I’m a Corporate Trainer, Academician, and{" "}
+                <span className="bolder">Founder of ProStrategic-H</span>, with
+                18+ years of experience in leadership development, communication
+                mastery, and transformational learning. I help professionals and
+                organizations unlock their true potential.
+              </p>
+              <div className="heroCta">
+                <p>Connect Now</p>
+                <Image
+                  src={`/images/cta-arrow.svg`}
+                  width={32}
+                  height={32}
+                  alt="arrow icon"
+                  className="ctaIcon"
+                />
+              </div>
             </div>
           </div>
           <div className="rightContainer">
